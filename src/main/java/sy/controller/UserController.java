7e00716169946e0c5f0d8545dcfc235271f4623e
vigorous <quick_to_bowl@163.com>
2015-12-17 17:17:32 +0800

    easyui

--- src/main/java/sy/controller/UserController.java
+++ src/main/java/sy/controller/UserController.java
@@ -1,8 +1,8 @@
 package sy.controller;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,23 +14,20 @@ import sy.service.UserServiceI;
 @RequestMapping("/userController")
 public class UserController {
 
+	@Resource
 	private UserServiceI userService;
 
-	public UserServiceI getUserService() {
-		return userService;
-	}
-
-	@Autowired
+	/*@Autowired
 	public void setUserService(UserServiceI userService) {
 		this.userService = userService;
-	}
+	}*/
 
 	@RequestMapping("/{id}/showUser")
 	public String showUser(@PathVariable String id, HttpServletRequest request) {
 		System.out.println("showUser...");
 		User u = userService.getUserById(id);
 		request.setAttribute("user", u);
-		return "showUser";
+		return "easyui_demo";
 	}
 
 }
